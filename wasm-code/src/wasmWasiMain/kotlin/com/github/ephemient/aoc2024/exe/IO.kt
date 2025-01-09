0991eb74cc45615c1058263ca772e00752753ef8
Alexey Illarionov <alexey@0xdc.ru>
2025-01-09 19:21:51 +0300

    Workaround for \r\n newlines on Windows

--- wasm-code/src/wasmWasiMain/kotlin/com/github/ephemient/aoc2024/exe/IO.kt
+++ wasm-code/src/wasmWasiMain/kotlin/com/github/ephemient/aoc2024/exe/IO.kt
@@ -1,3 +1,5 @@
 package com.github.ephemient.aoc2024.exe
 
-internal fun getDayInput(day: Int): String = readFile("/data/day$day.txt").decodeToString()
+internal fun getDayInput(day: Int): String = readFile("/data/day$day.txt")
+    .decodeToString()
+    .replace("\r\n", "\n")
